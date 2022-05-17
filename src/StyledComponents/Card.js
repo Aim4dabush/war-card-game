649c59b6840bba364d127d4af12b7cb8b8e62267
aim4dabush <edarasato@gmail.com>
2022-05-17 10:22:03 -0700

    added default background color for card

--- src/StyledComponents/Card.js
+++ src/StyledComponents/Card.js
@@ -1,7 +1,9 @@
 import styled from "styled-components";
+import { backgroundColor } from "./Variables";
 
 export const Card = styled.img`
   background-image: url("/images/playing-card-back.jpg");
+  background-color: ${backgroundColor};
   background-size: cover;
   border-radius: 7px;
   height: 350px;
